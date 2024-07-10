29e9970d6ed6630bb652d07e85cc58858161925c
Daniel Eduardo Jerónimo Jiménez <142690283+B0l48@users.noreply.github.com>
2024-07-10 13:43:02 -0600

    Crear Palabras.py

---
+++ palabras.py
@@ -0,0 +1,27 @@
+#Estos diccionarios serán utilizados en el juego de ahorcado para proporcionar palabras de diferentes categorías
+
+diccionario_2 = {
+    "equiposf1": [
+        "redbull", "ferrari", "mercedes", "alpine", "mclaren", "alfaromeo",
+        "astonmartin", "haas", "alphatauri", "williams"
+    ],
+    "pilotosf1": [
+        "russell", "gasly", "magnussen", "norris", "perez", "alonso",
+        "tsunoda", "sainz", "bottas", "sargeant"
+    ]
+}
+
+diccionario_1 = {
+    "equipos_futbol": [
+        "america", "pumas", "cruzazul", "guadalajara", "necaxa", "atlas",
+        "pachuca", "atleticosanluis", "puebla", "juarez", "queretaro",
+        "santos", "leon", "tigres", "mazatlan", "tijuana", "monterrey",
+        "toluca"
+    ],
+    "estadios": [
+        "azteca", "olimpicouniversitario", "jalisco", "bbva", "cuauhtemoc",
+        "akron", "universitario", "corregidora", "nemesiodiez", "leon",
+        "tsmcorona", "hidalgo", "caliente", "alfonsolastras ramirez",
+        "mazatlan", "victoria", "olimpicobenitojuarez"
+    ]
+}
